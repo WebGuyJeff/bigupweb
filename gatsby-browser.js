d978a06aa37382188daefce3b5f9a161b9adb19f
Jefferson <me@jeffersonreal.com>
2022-12-23 04:37:15 +0000

    added prismjs syntax highlighting

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,6 @@
 import '@fontsource/roboto-flex/variable-full.css'
+import '@fontsource/fira-code'
 import '@fontsource/montserrat/variable.css'
 import '@fontsource/montserrat/variable-italic.css'
 import './src/styles/global.scss'
+import './src/styles/wordpress.scss'
--- src/templates/WpPageTemplate/WpPageTemplate.js
+++ src/templates/WpPageTemplate/WpPageTemplate.js
@@ -3,8 +3,27 @@ import PropTypes from 'prop-types'
 import TitleBanner from 'components/banners/TitleBanner/TitleBanner'
 import MaxWidth from 'components/containers/MaxWidth/MaxWidth'
 import { pageContent } from './WpPageTemplate.module.scss'
+import Prism from 'prismjs'
+Prism.highlightAll()
 
 const WpPageTemplate = ( { title, excerpt, content } ) => {
+
+	//DEBUG
+	const code = `// This is a code block
+	const getData = graphql
+		query {
+			site {
+				siteMetadata {
+					siteTitle: title
+					siteDesc: description
+					image
+					author
+					siteUrl
+					twitterUsername
+				}
+			}
+		}
+	`
 
 	return (
 		<>
@@ -13,6 +32,11 @@ const WpPageTemplate = ( { title, excerpt, content } ) => {
 				subheading={ excerpt }
 			/>
 			<MaxWidth width="tight">
+				<pre>
+					<code className="language-javascript">
+						{ code }
+					</code>
+				</pre>
 				<div className={ pageContent }>
 					{ excerpt && (
 						<div dangerouslySetInnerHTML={ { __html: content } } />
